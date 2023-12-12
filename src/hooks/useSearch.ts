3ef3d56502fe373a85701ae64a678275ca565b1b
HY <harshy.167@gmail.com>
2023-12-12 22:34:14 +0530

    Initial Commit #2

---
+++ src/hooks/useSearch.ts
@@ -0,0 +1,49 @@
+import React from 'react';
+import { UserDataType } from './../types/userData.type';
+import { collection, onSnapshot, query, where } from 'firebase/firestore';
+import { auth, db } from '../firebase';
+
+export const useSearch = (input: string) => {
+  const [users, setUsers] = React.useState<UserDataType[]>();
+
+  const getSearchUser = async () => {
+    const q = auth.currentUser
+      ? query(
+          collection(db, 'users'),
+          where('username', '!=', auth.currentUser?.displayName),
+          where('username', '>=', input),
+          where('username', '<=', input + '\uf8ff')
+        )
+      : query(
+          collection(db, 'users'),
+          where('username', '>=', input),
+          where('username', '<=', input + '\uf8ff')
+        );
+
+    if (input.length > 0) {
+      onSnapshot(q, async (data) => {
+        try {
+          const dataa = data.docs.map((doc) => ({
+            ...(doc.data() as Record<string, unknown>),
+          })) as any;
+          console.log(dataa);
+          await setUsers(dataa);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      });
+    } else {
+      setUsers([]);
+    }
+  };
+
+  React.useEffect(() => {
+    if (input) {
+      (async () => {
+        await getSearchUser();
+      })();
+    }
+  }, [input]);
+
+  return { users, getSearchUser };
+};
